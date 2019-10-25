f1356924cf2a83f096d49fe4002b247c261b2e9a
Filip Hrisafov <filip.hrisafov@flowable.com>
2019-10-25 08:22:38 +0200

    Initial code

---
+++ src/test/java/com/example/democonfigurationproperties/DemoConfigurationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democonfigurationproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigurationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
